b294632ed21f02125b037c31b70971c02f842361
andrei <andrei_stoicu@yahoo.com>
2026-08-20 18:34:28 +0300

    template spring boot app

--- src/main/java/org/example/exception/NoBookFoundForDeleteException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class NoBookFoundForDeleteException extends RuntimeException {
-    public NoBookFoundForDeleteException(String message) {
-        super(message);
-    }
-}
